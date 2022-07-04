92dafa27c2d53c233ae1595dd5b481ffcebd0533
Lix Wxit <zinebbendjafer9@gmail.com>
2022-07-04 10:29:24 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,19 @@ module.exports = {
       sm: '375px',
       md: '765px',
     },
-    extend: {
-      colors: {
-        "darkGreen" : '#073b3a',
-        "lightGreen" : '#0b6e4f',
-        "lighterGreen" : '#4ADF86;',
-        "blue" : '#077187',
-        "textColor" : '#a3f7b5',
-      },
-
-      
-      boxShadow: {
-        "contentShadow": '5px 5px 60px -10px rgba(0, 0, 0, 0.3)',
-      }
+    colors: {
+      "darkGreen" : '#073b3a',
+      "lightGreen" : '#0b6e4f',
+      "lighterGreen" : '#4ADF86;',
+      "blue" : '#077187',
+      "textColor" : '#a3f7b5',
     },
+
+    
+    boxShadow: {
+      "contentShadow": '5px 5px 60px -10px rgba(0, 0, 0, 0.3)',
+    },
+    extend: {},
     
   },
   plugins: [],
