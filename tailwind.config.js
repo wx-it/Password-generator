a54bb65c1d3ba60c3f3aa58c303386a6bb886544
Lix Wxit <zinebbendjafer9@gmail.com>
2022-07-04 10:23:09 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  purge : ["./src/**/*.{html,js}"],
   theme: {
     screens:{
       sm: '375px',
